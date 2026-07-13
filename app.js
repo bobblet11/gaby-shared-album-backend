60549be5a8dafe8df3e90cb657fde3d36c1503b5
bobblet11 <benglover78@gmail.com>
2026-07-14 02:44:46 +0800

    fix

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const image_form_field = "image";
 app.get("/api/photo", async (req, res) => {
         try {
                 console.log("Request to load all images received");
-                const result = await db.query(`SELECT * FROM ${photosTable}`);
+                const result = await db.query(`SELECT * FROM ${photosTable} ORDER BY RANDOM()`);
                 res.json(result.rows);
         } catch (err) {
                 console.log(err);
