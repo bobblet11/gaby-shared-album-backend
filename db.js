172186d4e5dd1f14474267b52357ef612b1c31b0
bobblet11 <benglover78@gmail.com>
2026-07-14 17:52:33 +0800

    feat: added db url

--- db.js
+++ db.js
@@ -9,7 +9,13 @@ const pool = new Pool({
         database: process.env.DB_NAME,
 });
 
+const databaseUrl = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const encodedPassword = encodeURIComponent(process.env.DB_PASS);
+const encodedDatabaseUrl = `postgresql://${process.env.DB_USER}:${encodedPassword}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 module.exports = {
+        databaseUrl,
+        encodedDatabaseUrl,
         query: (text, params) => pool.query(text, params),
-        connect: () => pool.connect(),
+        connect: () => pool.connect()
 };
