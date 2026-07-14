4aebba69267f092316cfc47289017768e73dd13d
bobblet11 <benglover78@gmail.com>
2026-07-14 18:03:18 +0800

    feat: db migration. create account and assign photos to it

---
+++ migrations/20260714095830_update_photos_to_link_to_accounts.sql
@@ -0,0 +1,17 @@
+-- Migration: update_photos_to_link_to_accounts
+-- Created: 2026-07-14T09:58:30.344Z
+
+-- Add your migration SQL here
+
+
+BEGIN;
+
+INSERT INTO accounts (id, username, email, password_hash)
+VALUES (1, 'gabybrown8', 'gabybrown8@gmail.com', '-')
+ON CONFLICT (id) DO NOTHING;
+
+ALTER TABLE IF EXISTS photos
+    ADD COLUMN account_id INT NOT NULL DEFAULT 1,
+    ADD CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts 
+    
+COMMIT;
